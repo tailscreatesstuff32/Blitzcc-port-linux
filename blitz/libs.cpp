37ff9b42f238ef2ebdeaad16aecd8acafbad489d
tailsluver29 <66333352+tailscreatesstuff32@users.noreply.github.com>
2025-04-04 15:01:20 -0700

    Add files via upload

---
+++ blitz/libs.cpp
@@ -0,0 +1,26 @@
+#include "libs.h"
+
+int bcc_ver;
+int lnk_ver;
+int run_ver;
+int dbg_ver;
+
+
+Environ *runtimeEnviron;
+
+
+
+
+
+
+
+const char *openLibs(){
+
+
+}
+
+
+const char *linkLibs(){
+
+
+}
